6dcd61eb5e42d9cec5d5aaa185cbb2f2415fbe67
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-03-31 08:56:11 +0100

    added UI for error boundaries

---
+++ components/ErrorScreen.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const ErrorScreen = () => {
+  const router = useRouter();
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="flex flex-col items-center gap-5">
+        <h1>Failed to load</h1>
+
+        <button
+          onClick={() => router.back()}
+          className="px-3 py-3 w-52 mx-auto uppercase text-xs text-center text-main cursor-pointer rounded-3xl bg-gray-600 hover:bg-gray-700 my-2.5"
+        >
+          Head Back
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorScreen;
